b2c03081dc4aa1ac30dd7dbba4f707fb9b8e5380
Vitthal Mirji <vittal.mirji@bistasolutions.com>
2016-12-09 15:58:14 +0530

    Added explaination in comments

--- DesignPatterns/src/in/vitthal/designpatterns/decorator/Pizza.java
+++ DesignPatterns/src/in/vitthal/designpatterns/decorator/Pizza.java
@@ -1,8 +1,27 @@
 package in.vitthal.designpatterns.decorator;
 
+//Blueprint for classes that will have decorators
+
 public interface Pizza {
 	
 	public String getDescription();
 	public double getPrice();
-
+	
 }
+
+
+/* 
+public abstract class Pizza{
+
+	
+	public abstract void setDescription(String newDescription);
+	public abstract String getDescription();
+	
+	public abstract void setPrice(double newCost);
+	public abstract double getPrice();
+	
+	// I could use getter and setter methods for every 
+	// potential Pizza topping
+	
+}
+*/
--- DesignPatterns/src/in/vitthal/designpatterns/decorator/ToppingDecorator.java
+++ DesignPatterns/src/in/vitthal/designpatterns/decorator/ToppingDecorator.java
@@ -1,5 +1,8 @@
 package in.vitthal.designpatterns.decorator;
 
+//Has a "Has a" relationship with Pizza. This is an
+//Aggregation Relationship
+
 public abstract class ToppingDecorator implements Pizza{
 
 	protected Pizza temperoraryPizza;
@@ -7,6 +10,13 @@ public abstract class ToppingDecorator implements Pizza{
 	public ToppingDecorator() {
 		// TODO Auto-generated constructor stub
 	}
+
+	// Assigns the type instance to this attribute
+	// of a Pizza
+	
+	// All decorators can dynamically customize the Pizza
+	// instance PlainPizza because of this
+		
 	
 	public ToppingDecorator(Pizza pizza) {
 		temperoraryPizza = pizza;
